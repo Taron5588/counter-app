0bffd1cfea5fc4b0a563a4375f58411e3a278716
T. Karapetyan <taronvkarapetyan@gmail.com>
2023-06-25 16:47:45 +0400

    feat: add increase, decrease, reset functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import './App.css';
 function Counter() {
 
   const [count, setCount] = useState(0);
-  const [maxValue, setMaxValue] = useState(100);
+  const [maxValue, setMaxValue] = useState(20);
   const [minValue, setMinValue] = useState(0);
   const [step, setStep] = useState(1)
+
+  const increase = () => { if (count < maxValue) { setCount(count + step);}};
+
+  const decrease = () => {if (count > minValue) {setCount(count - step);}};
+
+  const reset = () => {setCount(0);};
 
 
 
